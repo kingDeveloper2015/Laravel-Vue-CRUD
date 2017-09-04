0fa6dceff0c4450c562a425d3ae538c80fbc76a4
paladin2 <kingofprogram1990@aol.com>
2017-09-05 06:06:52 +1000

    Finished project

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <div>
-      aaaa
       <transition name="fade">
         <router-view></router-view>
       </transition>
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,42 +1,35 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-/*
-
-
-
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
-*/
-require('./bootstrap');
-// window.Vue = require('vue');
 import Vue from 'vue';
-import App from './App.vue';
+
 import VueRouter from 'vue-router';
-import Example from './components/Example.vue';
 Vue.use(VueRouter);
 
 import VueAxios from 'vue-axios';
 import axios from 'axios';
-Vue.use(VueAxios,axios);
+Vue.use(VueAxios, axios);
 
-const routes = [{
-  name: 'Example',
-  path: '/',
-  component: Example
-}];
+import App from './App.vue';
+import CreateItem from './components/CreateItem.vue';
+import DisplayItem from './components/DisplayItem.vue';
+import EditItem from './components/EditItem.vue';
+
+const routes = [
+  {
+    name: 'CreateItem',
+    path: '/items/create',
+    component: CreateItem
+  },
+  {
+        name: 'DisplayItem',
+        path: '/',
+        component: DisplayItem
+  },
+  {
+      name: 'EditItem',
+      path: '/edit/:id',
+      component: EditItem
+   }
+];
 
 const router = new VueRouter({ mode: 'history', routes: routes});
-const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
-// Vue.component('example', require('./components/Example.vue'));
-//
-// const app = new Vue({
-//     el: '#app'
-// });
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
